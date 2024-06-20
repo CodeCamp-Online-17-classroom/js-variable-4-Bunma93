5cec63f80ce5bfd3a0ea541e6b0eea9554e221f7
Bunma93 <bunma93@gmail.com>
2024-06-20 15:11:41 +0700

    Js-lab4

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+const firstName = "ปฏิพงษศกร";
+let lastname = "บุญมา";
+var nickname = "James";
+const firstName = "พิสิด";
+let lastname = "ใจทน";
+var nickname = "ลีโอ";
+birthDate = "7 กุมภาพันธ์ 2544"
+
